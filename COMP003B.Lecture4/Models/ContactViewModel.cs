7f624f1202f95028339361943994bf0a345fb649
adobocoder <jcruz@adobo.dev>
2023-04-17 18:45:43 -0700

    Add project files.

---
+++ COMP003B.Lecture4/Models/ContactViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace COMP003B.Lecture4.Models
+{
+    public class ContactViewModel
+    {
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; }
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        [Phone]
+        public string Phone { get; set; }
+        [Required]
+        public int Age { get; set; }
+    }
+}
---
+++ COMP003B.Lecture4/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace COMP003B.Lecture4.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
